05b47d9093067d27a1d75f78f663329302fbaea0
Music <Music@LAPTOP-Q0D8EKOD>
2021-08-29 16:24:41 -0700

    commit new 7

--- SundayGit/src/main/java/com/git/sunday/SundayNew.java
+++ SundayGit/src/main/java/com/git/sunday/SundayNew.java
@@ -4,7 +4,9 @@ public class SundayNew {
 	
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("hello");
+		System.out.println("hi");
+		System.out.println("amen");
+		 
 	}
 
 }
